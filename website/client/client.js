1410374003d16f1eda1694081ae7c23aa439117f
Bjorn Tipling <bjorn@ambientchill.com>
2013-05-31 14:07:23 -0700

    Render line chart and other style fixes.

--- website/client/client.js
+++ website/client/client.js
@@ -2,6 +2,14 @@
  * fileOverview Client code etc.
  */
 
+var dataMap;
+
+/**
+ * Map for canvas data.
+ * @type {Object}
+ */
+dataMap = {};
+
 /**
  * @return {boolean}
  */
@@ -37,3 +45,111 @@ Template.dashboard.namesCount = function() {
   }
   return names[0].names.length;
 };
+
+Template.dashboard.recentMessages = function() {
+  var messages;
+  messages = Messages.find({}, {sort: {ts:-1}, limit: 20});
+  return Template.list(messages);
+};
+
+Template.dashboard.topAactive = function() {
+  var messages, countMap, results;
+  messages = Messages.find({});
+  countMap = {};
+  messages.forEach(function(message) {
+    if(!_.has(countMap, message.nick)) {
+      return countMap[message.nick] = 1;
+    }
+    countMap[message.nick] += 1;
+  });
+  results = _.map(countMap, function(count, nick) {
+    return {nick: nick, count: count};
+  });
+  results = _.sortBy(results, function(person) {
+    return person.count * -1;
+  });
+  console.log('results', results);
+  return Template.leaderboard(results);
+};
+
+Template.linechart.rendered = function() {
+  var id, element;
+  element = this.find('svg');
+  id = $(element).attr('data-id');
+  console.log('data', dataMap[id], element);
+  drawLine(dataMap[id], element);
+};
+
+Template.dashboard.online = function() {
+  var names, data;
+  names = Names.find({});
+  data = [];
+  names.forEach(function(nameset) {
+    data.push({
+      count: nameset.names.length,
+      ts: nameset.ts
+    });
+  });
+  dataMap.online = data;
+  return Template.linechart({id: 'online'});
+};
+
+/**
+ * @param {Array.<Object>} data
+ * @param {Element} element
+ */
+function drawLine(data, svg) {
+  var e, width, height, line, xValues, yValues, yAxis, xAxis;
+  width = 300;
+  height = 100;
+  e = d3.select(svg)
+    .attr('width', width)
+    .attr('height', height);
+  x = d3.time.scale().range([0, width]);
+  y = d3.scale.linear().range([height, 0]);
+  xValues = _.map(_.values(data), function(d) {
+    return d.ts;
+  });
+  yValues = _.map(_.values(data), function(d) {
+    return d.count;
+  });
+  x.domain([d3.min(xValues), d3.max(xValues)]);
+  y.domain([d3.min(yValues), d3.max(yValues)]);
+  line = d3.svg.line()
+    .interpolate('basis')
+    .x(function(d) {
+      console.log('ts', d.ts, width);
+      console.log('ts value', x(d.ts));
+      return x(d.ts);
+    })
+    .y(function(d) {
+      console.log('count', d.count, height);
+      console.log('count value', y(d.count));
+      return y(d.count);
+    })
+  yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+  xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+  e.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .text('Number online');
+  e.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+  e.append('path').attr('d', line(data))
+    .style('stroke', '#000')
+    .style('stroke-width', '1px')
+    .style('fill', 'none');
+
+
+}
