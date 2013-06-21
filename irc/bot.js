3b8d113b0adb3bcb62f6ded21b197a1928978080
Bjorn Tipling <bjorn@ambientchill.com>
2013-06-20 17:18:11 -0700

    Updating bot error reporting.

--- irc/bot.js
+++ irc/bot.js
@@ -3,7 +3,7 @@
 
   var irc, ircClient, config, channel, nick, secret, _,
   LIST_FETCH_INTERVAL, RECONNECT_WAIT, DDPClient, ddpclient,
-  ddpConnected;
+  ddpConnected, ircSay, ircAlert;
 
   irc = require('irc');
   DDPClient = require('ddp');
@@ -69,7 +69,10 @@
       });
     }
     ddpclient.connect(function(error) {
+      var msg;
+      msg = 'Error connecting to Meteor: ';
       if (error) {
+        ircAlert(msg + ' ' + error.toString());
         console.log('Error connecting to Meteor: ', error);
       }
       console.log('Connected to Meteor!');
@@ -80,6 +83,7 @@
       ddpConnected = false;
     });
     ddpclient.on('socket-error', function() {
+      ircAlert('Socket error!');
       console.log('socket error', arguments);
     });
   }
@@ -97,12 +101,16 @@
       console.log('Sending a message to meteor!', method, args);
       args.unshift(secret);
       ddpclient.call(method, args, function(err, result) {
+        var msg;
+        msg = 'Got an error from meteor';
         if (err) {
+          ircAlert(msg + ' ' + err.toString());
           return console.log('Got an error from meteor', err);
         }
         console.log('Method response success.');
         if (!_.isUndefined(result)) {
           console.log('Meteor call result:', result);
+          ircSay('Meteor call result: ' + result.toString());
         }
       });
     }
@@ -119,5 +127,23 @@
     }
     _.delay(getList, LIST_FETCH_INTERVAL);
   }
+
+  /**
+   * @param {string} msg
+   */
+  function ircSay_(msg) {
+    if (ircClient) {
+      ircClient.say(config.adminChannel, msg);
+    }
+  }
+  ircSay = _.throttle(ircSay_, 2000);
+
+  function ircAlert_(msg) {
+    if (ircClient) {
+      ircClient.say(config.adminChannel, msg);
+      ircClient.say(config.admin, msg);
+    }
+  }
+  ircAlert = _.throttle(ircAlert_, 2000);
 
 })();
